d077c7c4a7158166f620581db291f8e8f6822573
Ainur-sr <ainur-sr@yandex.ru>
2018-11-16 23:08:39 +0300

    fix commit 03

---
+++ src/main/java/ru/innopolis/stc12/bd/add_match_inf/MainAddMatchInf.java
@@ -0,0 +1,14 @@
+package ru.innopolis.stc12.bd.add_match_inf;
+
+public class MainAddMatchInf {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void addMatchInf(){
+
+
+    }
+
+}
