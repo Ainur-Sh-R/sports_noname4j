3dbdb295671d90ab395a5b827ceb486e95420ced
Ainur-sr <ainur-sr@yandex.ru>
2018-12-04 20:29:50 +0300

    finish commit

--- src/main/java/ru/innopolis/stc12/controller/BanUsersController.java
+++ src/main/java/ru/innopolis/stc12/controller/BanUsersController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.innopolis.stc12.bd.pojo.User;
 import ru.innopolis.stc12.service.ServiceBanUsers;
 
@@ -21,23 +22,26 @@ public class BanUsersController {
     }
 
     @RequestMapping(value = "/usersban", method = RequestMethod.GET)
-    String showUserBanList(Model model){
+    String showUserBanList(Model model) {
         model.addAttribute("userlist", serviceBanUsers.getUsers());
         return "usersban";
     }
 
     @RequestMapping(value = "/banUser", method = RequestMethod.POST)
-    public String banUser(@RequestParam (value = "userBanId") Integer userId) {
-        serviceBanUsers.banUser(userId);
-      return "usersban";
+    public String banUser(@RequestParam(value = "userBanId") Integer userId) {
+        if (userId != null) {
+            serviceBanUsers.banUser(userId);
+        }
+        return "redirect:/usersban";
+
     }
 
     @RequestMapping(value = "/banUserCancel", method = RequestMethod.POST)
-    public String banUserCancel(@RequestParam (value = "userBanId") Integer userId) {
+    public String banUserCancel(@RequestParam(value = "userBanId") Integer userId) {
         if (userId != null) {
             serviceBanUsers.banUserCancel(userId);
         }
-      return "usersban";
+        return "redirect:/usersban";
     }
 
 }
